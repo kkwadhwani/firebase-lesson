e68274fb1e93d1ac59be7f709cd167a5e8627bb1
Kunal Wadhwani <kkwadhwani@gmail.com>
2022-05-15 13:21:34 +0530

    Adding document

--- src/index.js
+++ src/index.js
@@ -1,21 +1,64 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, doc, getDoc, collection, addDoc } from 'firebase/firestore'
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  collection,
+  addDoc,
+} from "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBvlbWuqhZnKiuHjHLwjpFs3WT8W6quGLc",
-    authDomain: "react-native-a0092.firebaseapp.com",
-    projectId: "react-native-a0092",
-    storageBucket: "react-native-a0092.appspot.com",
-    messagingSenderId: "148185820101",
-    appId: "1:148185820101:web:a749c22fa4c34d6ab179ac",
-    measurementId: "G-SNZMXJYNGN"
-  };
+  apiKey: "AIzaSyBvlbWuqhZnKiuHjHLwjpFs3WT8W6quGLc",
+  authDomain: "react-native-a0092.firebaseapp.com",
+  projectId: "react-native-a0092",
+  storageBucket: "react-native-a0092.appspot.com",
+  messagingSenderId: "148185820101",
+  appId: "1:148185820101:web:a749c22fa4c34d6ab179ac",
+  measurementId: "G-SNZMXJYNGN",
+};
 
-  const app = initializeApp(firebaseConfig)
-  const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+//Adding document
+const c = collection(db, "restaurants");
+const data = {
+  name: "Banana Leaf",
+  year_started: 1990,
+  cuisine: ["Punjabi", "Rajasthani"],
+  location: "Prahalad Nagar",
+  closed: false,
+};
 
 
-  //Adding document
 
-  
+const form = document.getElementById("myform");
+const check = document.getElementById("closed");
+
+form.addEventListener("submit", addDocument);
+
+async function addDocument(e) {
+  e.preventDefault();
+
+  const name = form.name.value;
+  const year = form.year.value;
+  const totalCuisines = form.cuisine.value;
+
+  //split cuisine
+  const cuisine = totalCuisines.split(',')
+  const location = form.location.value
+  const isclosed = check.checked;
+
+  const data = {
+      name, year, cuisine, location, isclosed
+  }
+
+  try {
+    const docRef = await addDoc(c, data);
+    console.log("Document is set with ", docRef.id);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
